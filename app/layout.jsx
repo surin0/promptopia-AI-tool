68ad6eb72bced2921b1249e391ea4f5d2ffc9822
surin0 <atom598@proton.me>
2024-06-02 17:55:21 +0800

    Initial Commit

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import '@styles/globals.css';
+export const metadata ={
+    title: "Promptopia",
+    description: "Discover & Share AI prompts"
+}
+const RootLayout = () => {
+  return (
+    <html>
+        <body lang="en">
+            <div className="main">
+                <div className="gradient"/>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+            </body>
+    </html>
+    
+  )
+}
+
+export default RootLayout;
